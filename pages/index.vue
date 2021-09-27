3d260854f9b4be25b7519567c0f6e3b16b570371
ilsrbn <serbini271@gmail.com>
2021-09-27 21:07:22 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <baseDatepicker />
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss">
+body {
+  background: #0F1215;
+  .app {
+    width: 300px;
+  }
+}
+</style>
